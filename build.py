3f9b273091248aae85e7cec997e9805be252d93c
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-07-02 22:01:48 +0530

    Fix 2017
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ import os
 url = 'http://localhost:8000'
 
 if os.environ.get('PYCON_ENV') == 'PROD':
-    url = 'https://pune.pycon.org'
+    url = 'https://pune.pycon.org/2017'
 
 
 if __name__ == "__main__":
