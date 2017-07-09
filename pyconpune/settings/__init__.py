9e6cddac208ad7b3b166ec8f68f7a83bd0704eba
Saptak Sengupta <saptak013@gmail.com>
2017-07-09 21:40:37 +0530

    tshirt: Add Tshirt and UserTshirt model

--- pyconpune/settings/__init__.py
+++ pyconpune/settings/__init__.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     'root',
     'ticket',
+    'tshirt',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ tshirt/models.py
@@ -0,0 +1,41 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+
+from symposion.conference.models import Conference
+
+from root.models import Base
+
+class Tshirt(Base):
+    """ Model to store the different types of tshirt. """
+
+    gender = models.CharField(_("gender"), max_length=255)
+    limit = models.PositiveIntegerField(_("limit"), default=0)
+    price = models.PositiveIntegerField(_("price"), default=0, db_index=True)
+    conference = models.ForeignKey(Conference, verbose_name=_("conference"))
+
+    class Meta:
+        verbose_name = _("tshirt")
+        verbose_name_plural = _("tshirts")
+
+    def __unicode(self):
+        return u"%s: %s" % (self.conference.title, self.gender)
+
+
+class UserTshirt(Base):
+    """ Model for maitaining the tshirt order entry for all the Users. """
+
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    size = models.CharField(_("size"), max_length=5)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tshirt = models.ForeignKey(Tshirt, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("user tshirt")
+        verbose_name_plural = _("tshirt")
+        ordering = ['-timestamp']
+
+    def __unicode__(self):
+        return u'%s:%s:%s' % (self.user.username, self.tshirt.gender, self.size)
