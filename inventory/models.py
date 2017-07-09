d53b05f648053d46c6b4b7353d9acb96d6c18179
Saptak Sengupta <saptak013@gmail.com>
2017-07-09 22:12:00 +0530

    inventory: Move size from UserTshirt to Tshirt

--- inventory/models.py
+++ inventory/models.py
@@ -12,6 +12,7 @@ class Tshirt(Base):
     """ Model to store the different types of tshirt. """
 
     gender = models.CharField(_("gender"), max_length=255)
+    size = models.CharField(_("size"), max_length=5)
     limit = models.PositiveIntegerField(_("limit"), default=0)
     price = models.PositiveIntegerField(_("price"), default=0, db_index=True)
     conference = models.ForeignKey(Conference, verbose_name=_("conference"))
@@ -28,7 +29,6 @@ class UserTshirt(Base):
     """ Model for maitaining the tshirt order entry for all the Users. """
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    size = models.CharField(_("size"), max_length=5)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tshirt = models.ForeignKey(Tshirt, on_delete=models.CASCADE)
 
