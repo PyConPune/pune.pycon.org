8ecacc30e6ab569d4a6c5bcb38f0c111f185a13a
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-07-15 21:26:02 +0530

    static: Add the static files and the related configuration(s)
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- root/urls.py
+++ root/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
+
 
 from root.views import homepage
 
@@ -6,3 +9,6 @@ urlpatterns = [
     url(r'^$', homepage,
         name="homepage"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
