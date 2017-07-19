e3b4ad191335383d829272163c7364a9180c5f71
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-07-19 12:46:21 +0530

    tickets: Show the green ticket on the highlighted card
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- pyconpune/staticfiles/js/tickets/ticket.js
+++ pyconpune/staticfiles/js/tickets/ticket.js
@@ -6,11 +6,13 @@
             children.each( function (i) {
                 if ($(this).attr('id') != id) {
                     $(this).addClass('ticket-disabled');
+                    $(this).find('.fa-check').addClass('hidden');
                 }
                 if ($(this).attr('id') === id) {
                     if ($(this).hasClass('ticket-disabled')) {
                         $(this).removeClass('ticket-disabled');
                     }
+                    $(this).find('.fa-check').removeClass('hidden');
                 }
             })
         })
