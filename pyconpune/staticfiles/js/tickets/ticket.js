6bcce30fb6644d210582b8322c07349750a8a36e
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2017-07-19 13:06:25 +0530

    tickets: Add the effects for the second layer
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- pyconpune/staticfiles/js/tickets/ticket.js
+++ pyconpune/staticfiles/js/tickets/ticket.js
@@ -1,5 +1,5 @@
     $(document).ready( function() {
-        $('.ticket').click(function() {
+        $('.p-ticket').click(function() {
             $this = $(this);
             var id = $this.attr('id');
             var children = $this.parent().children()
@@ -14,6 +14,18 @@
                     }
                     $(this).find('.fa-check').removeClass('hidden');
                 }
-            })
+            });
+            $('html, body').animate({
+                scrollTop: $("#ticket-row-2").offset().top - 70
+            }, 1000);
+        });
+
+        $('.a-ticket').click(function() {
+            $this = $(this);
+            if ($(this).find('.fa-check').hasClass('hidden')) {
+                $this.find('.fa-check').removeClass('hidden');
+            } else {
+                $this.find('.fa-check').addClass('hidden');
+            }
         })
     });
