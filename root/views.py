1b521b7f7a4f39c6196e0b692bb7255e79c02058
Vaibhav Lohani <vaibhav241196@gmail.com>
2017-09-20 23:55:30 +0530

    1. Adds section for volunteers list, list is received via backend. 2. Changes volunteer button style. 3. Fixes broken footer. 4. Adds link to homepage in the navbar logo.

--- root/views.py
+++ root/views.py
@@ -6,13 +6,41 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def homepage(request):
     return render(request, template_name='index.html', context={'page': 'home'})
-
-
+    
 def coc(request):
     return render(request, template_name='coc.html', context={'page': 'coc'})
 
 def volunteer(request):
-    return render(request, template_name='volunteer.html', context={'page': 'volunteer'})
+    # Dummy List of Volunteers, will be fetched from DB later
+    list_of_volunteers = [
+        {"name": "Volunteer 1", "description": "Enthusiastic Volunteer",
+         "image": "img/anonymous.png", "github": "http://github.com",
+         "twitter": "http://twitter.com"
+        },
+        {"name": "Volunteer 2", "description": "Enthusiastic Volunteer",
+         "image": "img/anonymous.png", "github": "http://github.com",
+         "twitter": "http://twitter.com"
+        },
+        {"name": "Volunteer 3", "description": "Enthusiastic Volunteer",
+         "image": "img/anonymous.png", "github": "http://github.com",
+         "twitter": "http://twitter.com"
+        },
+        {"name": "Volunteer 4", "description": "Enthusiastic Volunteer",
+         "image": "img/anonymous.png", "github": "http://github.com",
+         "twitter": "http://twitter.com"
+        },
+        {"name": "Volunteer 5", "description": "Enthusiastic Volunteer",
+         "image": "img/anonymous.png", "github": "http://github.com",
+         "twitter": "http://twitter.com"
+        },
+        {"name": "Volunteer 6", "description": "Enthusiastic Volunteer",
+         "image": "img/anonymous.png", "github": "http://github.com",
+         "twitter": "http://twitter.com"
+        }
+    ]
+
+    return render(request, template_name='volunteer.html',
+                context={'page': 'volunteer', 'volunteer_list': list_of_volunteers })
 
 def about(request):
     return render(request, 'about.html', context={'page': 'about'})
