4d7b0d1c87d727cad6b2250705a2b32a91a7c78e
Omkar Pathak <omkarpathak27@gmail.com>
2017-11-03 10:09:50 +0530

    Added Traveling info

---
+++ cauth/migrations/0006_auto_20171103_0204.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cauth', '0005_auto_20171010_1534'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userprofile',
+            name='age_group',
+            field=models.CharField(verbose_name='age group', max_length=255, choices=[('1', 'Less than 10'), ('2', '11-17'), ('3', '18-24'), ('4', '25-34'), ('5', '35-44'), ('6', '45 and over')]),
+        ),
+        migrations.AlterField(
+            model_name='userprofile',
+            name='tshirt_size',
+            field=models.CharField(verbose_name='tshirt size', max_length=255, choices=[('MXS', 'Male: Xtra Small'), ('MS', 'Male: Small'), ('MM', 'Male: Medium'), ('ML', 'Male: Large'), ('MXL', 'Male: Xtra Large'), ('MXXL', 'Male: Double Xtra Large'), ('FXS', 'Female: Xtra Small'), ('FS', 'Female: Small'), ('FM', 'Female: Medium'), ('FL', 'Female: Large'), ('FXL', 'Female: Xtra Large'), ('FXXL', 'Female: Double Xtra Large')]),
+        ),
+    ]
