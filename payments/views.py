82d0b177dcf149f9eab038156b36f95617f9eb89
Saptak Sengupta <saptak013@gmail.com>
2017-11-20 23:08:10 +0530

    payment: Sync only non paid invoices
    Signed-off-by: Saptak Sengupta <saptak013@gmail.com>

--- payments/views.py
+++ payments/views.py
@@ -37,20 +37,21 @@ def sync(request):
     payment = RazorpayPayments(keys.api_key, keys.api_secret)
     invoices = Invoice.objects.all()
     for invoice in invoices:
-        invoice_details = payment.fetch_invoices(invoice.invoice_id)
-        invoice.status = invoice_details['status']
-        invoice.save()
-        if invoice.status == 'paid':
-            orders = Order.objects.filter(order_id=invoice_details['order_id'])
-            if len(orders) == 0:
-                order_details = payment.fetch_orders(
-                    invoice_details['order_id'])
-                payment.save_order(order_details)
-            if invoice_details['payment_id']:
-                payments = Payment.objects.filter(payment_id=invoice_details['payment_id'])
-                if len(payments) == 0:
-                    payment_details = payment.fetch_payment(invoice_details['payment_id'])
-                    payment.save_payment(payment_details)
+        if invoice.status != 'paid':
+            invoice_details = payment.fetch_invoices(invoice.invoice_id)
+            invoice.status = invoice_details['status']
+            invoice.save()
+            if invoice.status == 'paid':
+                orders = Order.objects.filter(order_id=invoice_details['order_id'])
+                if len(orders) == 0:
+                    order_details = payment.fetch_orders(
+                        invoice_details['order_id'])
+                    payment.save_order(order_details)
+                if invoice_details['payment_id']:
+                    payments = Payment.objects.filter(payment_id=invoice_details['payment_id'])
+                    if len(payments) == 0:
+                        payment_details = payment.fetch_payment(invoice_details['payment_id'])
+                        payment.save_payment(payment_details)
 
     return JsonResponse({"message": "synced"})
 
